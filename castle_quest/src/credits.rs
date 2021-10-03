eef7bc89e957afdbef9f45ce949f6aaf19bd20ff
Kaleb Raymond <kwr7@pitt.edu>
2021-10-03 18:45:53 -0400

    Moved centered_rect macro to its own file

--- castle_quest/src/credits.rs
+++ castle_quest/src/credits.rs
@@ -9,36 +9,6 @@ use crate::SDLCore;
 
 const CREDITS_TIMEOUT: u64 = 4500;
 
-/// Centered Rect macro: constructs a rect in the middle of the screen with the designated width/height
-macro_rules! centered_rect {
-	($core: ident, $width: expr, $height: expr) => {
-		Rect::new(
-			($core.cam.width()/2 -$width/2) as i32,
-			($core.cam.height()/2 - $height/2) as i32,
-			$width,
-			$height,
-		);
-	};
-	// vertically center a rect; centered_rect!(core, right, _, w, h)
-	($core: ident, $right: expr, _, $width: expr, $height: expr) => {
-		Rect::new(
-			$right,
-			($core.cam.height()/2 - $height/2) as i32,
-			$width,
-			$height,
-		);
-	};
-	// horizontally center a rect; centered_rect!(core, _, right, w, h)
-	($core: ident, _, $top: expr, $width: expr, $height: expr) => {
-		Rect::new(
-			($core.cam.width()/2 -$width/2) as i32,
-			$top,
-			$width,
-			$height,
-		);
-	};
-}
-
 /// Credits page macro: surrounds the provided "closure" (not really a closure) with canvas present() and thread::sleep calls
 macro_rules! credits_page {
 	($core: ident, $closure: expr) => {
--- castle_quest/src/main.rs
+++ castle_quest/src/main.rs
@@ -6,6 +6,8 @@ const CAM_H: u32 = 720;
 
 use sdl2::rect::Rect;
 
+#[macro_use] mod sdl_macros;
+
 mod credits;
 
 pub struct SDLCore {
@@ -60,14 +62,16 @@ fn init_sdl_core(vsync:bool) -> Result<SDLCore, String> {
 
 	let texture_creator = wincan.texture_creator();
 
-	Ok( SDLCore{
+	Ok( 
+		SDLCore{
 			sdl_ctx,
 			ttf_ctx,
 			wincan,
 			event_pump,
 			cam,
 			texture_creator,
-	})
+		}
+	)
 }
 
 fn main() {
