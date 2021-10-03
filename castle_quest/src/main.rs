1631916d9509e9e83382afe61b3c11d8332bc1d3
AlexKwi <ajk175@pitt.edu>
2021-10-03 13:30:37 -0400

    Added initial credit code with my credit image

--- castle_quest/src/main.rs
+++ castle_quest/src/main.rs
@@ -43,14 +43,87 @@ fn run(vsync:bool) -> Result<(), String> {
 	let mut wincan = wincan.build()
 		.map_err(|e| e.to_string())?;
 
-	let event_pump = sdl_cxt.event_pump()?;
+	let _event_pump = sdl_cxt.event_pump()?;
 
-	let cam = Rect::new(0, 0, CAM_W, CAM_H);
+	let _cam = Rect::new(0, 0, CAM_W, CAM_H);
 
 	let texture_creator = wincan.texture_creator();
 	
 	{ //Credits
+		let bold_font = ttf_cxt.load_font("fonts/OpenSans-Bold.ttf", 32)?; //From https://www.fontsquirrel.com/fonts/open-sans
+		let regular_font = ttf_cxt.load_font("fonts/OpenSans-Regular.ttf", 16)?; //From https://www.fontsquirrel.com/fonts/open-sans
+
+		wincan.set_draw_color(Color::RGBA(63, 191, 191, 128));
+		wincan.clear();
+
+		//Game title
+		let text_surface = bold_font.render("Castle Quest")
+								.blended_wrapped(Color::RGBA(0, 0, 0, 96), 100) //Black font
+								.map_err(|e| e.to_string())?;
+								
+		let text_texture = texture_creator.create_texture_from_surface(&text_surface) 
+											.map_err(|e| e.to_string())?;
 		
+		wincan.copy(&text_texture, None, None)?;
+		wincan.present();
+
+		thread::sleep(Duration::from_millis(TIMEOUT));
+		wincan.set_draw_color(Color::RGBA(60, 163, 62, 128));
+		wincan.clear();
+
+		//AI Team centered
+		let w = 500;
+		let r = Rect::new((CAM_W/2 - w/2) as i32, (CAM_H /2 - w/2) as i32, w, w);
+
+
+		let text_surface = regular_font.render("AI Subteam")
+								.blended_wrapped(Color::RGBA(0, 0, 0, 96), 160) //Black font
+								.map_err(|e| e.to_string())?;
+								
+		let text_texture = texture_creator.create_texture_from_surface(&text_surface) 
+											.map_err(|e| e.to_string())?;
+		
+		wincan.copy(&text_texture, None, r)?;
+		wincan.present();
+
+		thread::sleep(Duration::from_millis(TIMEOUT));
+		wincan.set_draw_color(Color::RGBA(201, 196, 196, 128));
+		wincan.clear();
+
+		//Alex Kwiatkowski Credit Image
+		let alex_credit = texture_creator.load_texture("images/AlexKCreditImageWithText.png")?;
+		wincan.copy(&alex_credit, None, None)?;
+		wincan.present();
+
+		//thread::sleep(Duration::from_millis(TIMEOUT));
+		//wincan.clear();
+
+		//........
+
+		thread::sleep(Duration::from_millis(TIMEOUT));
+		wincan.set_draw_color(Color::RGBA(238, 46, 21, 128));
+		wincan.clear();
+		
+		let text_surface = regular_font.render("Networking Subteam")
+								.blended_wrapped(Color::RGBA(0, 0, 0, 96), 320) //Black font
+								.map_err(|e| e.to_string())?;
+								
+		let text_texture = texture_creator.create_texture_from_surface(&text_surface) 
+											.map_err(|e| e.to_string())?;
+		
+		wincan.copy(&text_texture, None, r)?;
+		wincan.present();
+
+		// Note SDL has a timer subsystem, but the Rust SDL bindings recommend
+		// the use of std::thread::sleep and std::time instead
+		thread::sleep(Duration::from_millis(TIMEOUT));
+
+		//self.core.wincan.copy(&tux, None, None)?;
+		wincan.present();
+
+		thread::sleep(Duration::from_millis(TIMEOUT));
+
+		Ok(())
 	}
 }
 
