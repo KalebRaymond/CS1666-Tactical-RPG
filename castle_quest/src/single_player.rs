6c2656e6cd4d962eb55bf0291731553fe485c646
AlexKwi <ajk175@pitt.edu>
2021-10-10 11:43:18 -0400

    Added very basic counter to keep track of player turn and output whose turn it currently is; this is just until we actually decide how we want to represent players.

--- castle_quest/src/single_player.rs
+++ castle_quest/src/single_player.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use sdl2::event::Event;
 use sdl2::image::LoadTexture;
 use sdl2::keyboard::Keycode;
@@ -8,8 +10,12 @@ use crate::GameState;
 use crate::pixel_coordinates::PixelCoordinates;
 use crate::SDLCore;
 use crate::TILE_SIZE;
+use crate::CAM_W;
+use crate::CAM_H;
 
 pub fn single_player(core: &mut SDLCore) -> Result<GameState, String> {
+	let mut current_player = 0; //Very basic counter to keep track of player turn (will be changed to something more powerful later on)
+	
 	//Basic mock map, 48x48 2d vector filled with 1s
 	let mut map: Vec<Vec<u32>> = vec![vec![1; 48]; 48];
 	let map_width = map[0].len();
@@ -28,6 +34,24 @@ pub fn single_player(core: &mut SDLCore) -> Result<GameState, String> {
 				Event::Quit{..} | Event::KeyDown{keycode: Some(Keycode::Escape), ..} => break 'gameloop,
 				_ => {},
 			}
+		}
+
+		let keystate: HashSet<Keycode> = core.event_pump
+				.keyboard_state()
+				.pressed_scancodes()
+				.filter_map(Keycode::from_scancode)
+				.collect();
+		
+		if keystate.contains(&Keycode::Backspace) {
+			if current_player == 0 {
+				println!("Player 1");
+			} else if current_player == 1 {
+				println!("Player 2");
+			} else {
+				println!("Barbarians");
+				current_player = -1;
+			}
+			current_player += 1;
 		}
 
 		//Draw tiles & sprites
