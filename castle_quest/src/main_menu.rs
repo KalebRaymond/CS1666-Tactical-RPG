7a40358d98739c492bb30a11448f5e14b3d6054e
JakeBaumbaugh <jake.baumbaugh@gmail.com>
2021-10-12 15:15:56 -0400

    displaying text in textbox

--- castle_quest/src/main_menu.rs
+++ castle_quest/src/main_menu.rs
@@ -10,6 +10,9 @@ pub fn main_menu(core: &mut SDLCore) -> Result<GameState, String> {
 	let single_player_button = centered_rect!(core, _, 720/4, 100, 100);
 	let credit_button = centered_rect!(core, _, 3*720/4, 100, 100);
 	let join_code_textbox = Rect::new(750, 200, 400, 60);
+	let mut join_code = "test";
+
+	let regular_font = core.ttf_ctx.load_font("fonts/OpenSans-Regular.ttf", 32)?; //From https://www.fontsquirrel.com/fonts/open-sans
 
 	'menuloop: loop {
 		let mouse_state: MouseState = core.event_pump.mouse_state();
@@ -43,6 +46,19 @@ pub fn main_menu(core: &mut SDLCore) -> Result<GameState, String> {
 		
 		core.wincan.set_draw_color(Color::RGBA(255,255,255,255));
 		core.wincan.draw_rect(join_code_textbox)?;
+		let text_surface = regular_font.render(join_code)
+			.blended(Color::RGBA(255,255,255,255))
+			.map_err(|e| e.to_string())?;
+		let text_texture = core.texture_creator.create_texture_from_surface(&text_surface)
+			.map_err(|e| e.to_string())?;
+		let text_size = regular_font.size_of(join_code);
+		match text_size {
+			Ok((w, h)) => {
+				println!("{},{}", w, h);
+				core.wincan.copy(&text_texture, None, Rect::new(760, 200 + (60-h as i32)/2, w, h))?;
+			},
+			_ => {},
+		}
 
 		core.wincan.set_draw_color(Color::RGBA(0,255,0,255));
 		core.wincan.draw_rect(credit_button)?;
