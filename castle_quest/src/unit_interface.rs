938804ba7387b3f9a366a7b97c0ce14e474dda66
JakeBaumbaugh <jake.baumbaugh@gmail.com>
2021-10-14 14:26:49 -0400

    attempt to create a sprite for scroll

--- castle_quest/src/unit_interface.rs
+++ castle_quest/src/unit_interface.rs
@@ -13,22 +13,34 @@ pub struct UnitInterface<'a> {
     pub x: u32,
     pub y: u32,
     txt: Vec<&'a str>,
+    texture: Option<Texture<'a>>,
     anim_progress: f32,
     anim_state: AnimState,
 }
 
-impl UnitInterface<'_> {
-    pub fn new<'a>(i: u32, j: u32, t: Vec<&'a str>) -> UnitInterface<'a> {
+impl<'a> UnitInterface<'a> {
+    pub fn new(i: u32, j: u32, t: Vec<&'a str>) -> UnitInterface<'a> {
         UnitInterface { 
             x: j * crate::TILE_SIZE,
             y: i * crate::TILE_SIZE,
             txt: t,
+            texture: None,
             anim_progress: 0.0,
             anim_state: AnimState::Open,
         }
     }
 
-    pub fn get_texture(core: &mut SDLCore) -> Result<Texture, String> {
-        core.texture_creator.load_texture("images/interface/unit_interface.png")
+    pub fn get_texture(&self, core: &mut SDLCore) -> Option<Texture> {
+        self.texture
+    }
+
+    fn generate_texture(&self, core: &mut SDLCore) {
+        let base_texture = core.texture_creator.load_texture("images/interface/unit_interface.png");
+        self.texture = match base_texture {
+            Ok(spritesheet) => {
+                core.texture_creator.create_texture_target(None, 64, 32+16*self.txt.len() as u32).ok()
+            },
+            _ => None
+        }
     }
 }
