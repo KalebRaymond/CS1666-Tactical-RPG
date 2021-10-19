9023712216b676da0752eecbda069a876f9f1359
JakeBaumbaugh <jake.baumbaugh@gmail.com>
2021-10-19 14:53:58 -0400

    draw interface beside tile in desired spot

--- src/unit_interface.rs
+++ src/unit_interface.rs
@@ -1,5 +1,7 @@
+use sdl2::pixels::Color;
 use sdl2::rect::Rect;
-use sdl2::render::Texture;
+use sdl2::render::{Texture,TextureCreator};
+use sdl2::ttf::Font;
 
 use crate::SDLCore;
 
@@ -21,8 +23,8 @@ pub struct UnitInterface<'a> {
 impl<'a> UnitInterface<'a> {
     pub fn new(i: u32, j: u32, t: Vec<&'a str>, tex: &'a Texture<'a>) -> UnitInterface<'a> {
         UnitInterface { 
-            x: (j * crate::TILE_SIZE) as i32,
-            y: (i * crate::TILE_SIZE) as i32,
+            x: ((j-2) * crate::TILE_SIZE) as i32,
+            y: ((i-1) * crate::TILE_SIZE) as i32,
             txt: t,
             texture: Some(tex),
             anim_progress: 0.0,
@@ -30,12 +32,16 @@ impl<'a> UnitInterface<'a> {
         }
     }
 
-    pub fn draw(&self, core: &mut SDLCore) {
+    pub fn draw(&self, core: &mut SDLCore) -> Result<(), String> {
         match &self.texture {
             Some(texture) => {
-                core.wincan.copy(texture, None, Rect::new(self.x,self.y,64,64));
+                core.wincan.copy(texture, Rect::new(0,0,64,16), Rect::new(self.x,self.y,64,16))?;
+                core.wincan.copy(texture, Rect::new(0,16,64,16), Rect::new(self.x,self.y+16,64,16))?;
+                core.wincan.copy(texture, Rect::new(0,16,64,16), Rect::new(self.x,self.y+32,64,16))?;
+                core.wincan.copy(texture, Rect::new(0,32,64,16), Rect::new(self.x,self.y+48,64,16))?;
+                Ok(())
             },
-            _ => {},
+            _ => { Err("Texture not defined.".to_string()) },
         }
     }
 }
