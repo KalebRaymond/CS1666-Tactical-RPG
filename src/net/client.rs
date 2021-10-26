b914cc838033d393e0cb0bbf9366f2bb3aaabb2d
James Fenn <me@jfenn.me>
2021-10-26 19:12:16 -0400

    collect join code from main menu

--- src/net/client.rs
+++ src/net/client.rs
@@ -2,6 +2,17 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
+static mut CODE: Option<u32> = None;
+
+// sets a static CODE variable representing the multiplayer room to join
+//   if Some(code) -> joins a room with code
+//   if None       -> creates a new room
+pub fn set_code(code: Option<u32>) {
+	unsafe {
+		CODE = code;
+	}
+}
+
 pub struct Client {
 	stream: TcpStream,
 }
