e21fc3f47d6f216199fb19e9daa51ade319152f6
JakeBaumbaugh <jake.baumbaugh@gmail.com>
2021-10-26 01:23:43 -0400

    drawing text onto scroll

--- src/unit_interface.rs
+++ src/unit_interface.rs
@@ -41,12 +41,17 @@ impl<'a> UnitInterface<'a> {
                 core.wincan.copy(texture, Rect::new(0,16,64,16), Rect::new(self.x,self.y+32,64,16))?;
                 core.wincan.copy(texture, Rect::new(0,32,64,16), Rect::new(self.x,self.y+48,64,16))?;
 
-                let font = core.ttf_ctx.load_font("fonts/OpenSans-Regular.ttf", 16)?;
+                let font = core.ttf_ctx.load_font("fonts/OpenSans-Regular.ttf", 10)?;
                 for (i, text) in self.txt.iter().enumerate() {
                     let (text_w, text_h) = font.size_of(text)
                         .map_err( |e| e.to_string() )?;
                     let text_ratio = text_w as f32 / text_h as f32;
-                    //println!("{}/{} = {}", text_w, text_h, text_ratio);
+                    let text_surface = font.render(text)
+                        .blended_wrapped(Color::RGBA(0, 0, 0, 0), 320)
+                        .map_err(|e| e.to_string())?;
+                    let text_texture = texture_creator.create_texture_from_surface(&text_surface)
+                        .map_err(|e| e.to_string())?;
+                    core.wincan.copy(&text_texture, None, Rect::new(self.x+10, self.y+16*(i+1)as i32, 16*text_w/text_h, 16));
                 }
 
                 Ok(())
