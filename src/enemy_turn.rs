a17c675ba5e22e0b3fccf3de3fbf970f853db919
AlexKwi <ajk175@pitt.edu>
2021-11-08 13:08:16 -0500

    Modifying map initialization to initialize important coordinates for heuristics

--- src/enemy_turn.rs
+++ src/enemy_turn.rs
@@ -6,7 +6,7 @@ use crate::pixel_coordinates::PixelCoordinates;
 use crate::unit::{Team, Unit};
 use crate::turn_banner::TurnBanner;
 
-pub fn handle_enemy_turn<'a>(p2_units: &mut HashMap<(u32, u32), Unit<'a>>, p1_units: &mut HashMap<(u32, u32), Unit<'a>>, barbarian_units: &mut HashMap<(u32, u32), Unit<'a>>, game_map: &mut GameMap, turn_banner: &mut TurnBanner, current_player: &mut Team) {
+pub fn handle_enemy_turn<'a>(p2_units: &mut HashMap<(u32, u32), Unit<'a>>, p1_units: &mut HashMap<(u32, u32), Unit<'a>>, barbarian_units: &mut HashMap<(u32, u32), Unit<'a>>, game_map: &mut GameMap, turn_banner: &mut TurnBanner, current_player: &mut Team, p2_castle: &(u32, u32), p1_castle: &(u32, u32), camp_coords: &Vec<(u32, u32)>) {
     if !turn_banner.banner_visible {
         //End turn
         *current_player = Team::Barbarians;
