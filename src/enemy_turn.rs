30e7268af4aa568bbc9eb3afe1d592bd2432dbc6
AlexKwi <ajk175@pitt.edu>
2021-11-08 14:38:41 -0500

    Added calculation of current states value

--- src/enemy_turn.rs
+++ src/enemy_turn.rs
@@ -7,9 +7,10 @@ use crate::unit::{Team, Unit};
 use crate::turn_banner::TurnBanner;
 
 const MIN_DISTANCE: i32 = 5; // Defines the minimum distance a unit can be from an objective to be considered near it
-const DEFENDING_WEIGHT: f32 = 0.5;
-const SIEGING_WEIGHT: f32 = 0.5;
-const CAMP_WEIGHT: f32 = 0.5;
+const DEFENDING_WEIGHT: f64 = -0.5;
+const SIEGING_WEIGHT: f64 = -0.75;
+const CAMP_WEIGHT: f64 = -0.25;
+const ATTACK_VALUE: f64 = 10.0;
 
 pub fn handle_enemy_turn<'a>(p2_units: &mut HashMap<(u32, u32), Unit<'a>>, p1_units: &mut HashMap<(u32, u32), Unit<'a>>, barbarian_units: &mut HashMap<(u32, u32), Unit<'a>>, game_map: &mut GameMap, turn_banner: &mut TurnBanner, current_player: &mut Team, p2_castle: &(u32, u32), p1_castle: &(u32, u32), camp_coords: &Vec<(u32, u32)>) {
     if !turn_banner.banner_visible {
@@ -25,12 +26,14 @@ pub fn handle_enemy_turn<'a>(p2_units: &mut HashMap<(u32, u32), Unit<'a>>, p1_un
     }
 }
 
-pub fn evaluate_current_position<'a> (p2_units: &HashMap<(u32, u32), Unit<'a>>, game_map: &mut GameMap, p2_castle: &(u32, u32), p1_castle: &(u32, u32), camp_coords: &Vec<(u32, u32)>) -> u32 {
-    let mut total_value: u32 = 0;
+pub fn evaluate_current_position<'a> (p2_units: &HashMap<(u32, u32), Unit<'a>>, game_map: &mut GameMap, p2_castle: &(u32, u32), p1_castle: &(u32, u32), camp_coords: &Vec<(u32, u32)>) -> f64 {
+    let mut total_value: f64 = 0.0;
     let mut units_defending: u32 = 0; //Units near own castle 
     let mut units_sieging: u32 = 0; //Units near enemy castle
     let mut units_near_camp: u32 = 0;
     let mut units_able_to_attack: u32 = 0;
+
+    println!("Current Constants:\nMinimum Distance from Objectives: {}, Defending Weight: {}, Sieging Weight: {}, Camp Weight: {}, Value from Attack: {}\n", MIN_DISTANCE, DEFENDING_WEIGHT, SIEGING_WEIGHT, CAMP_WEIGHT, ATTACK_VALUE);
 
     for unit in p2_units.values() {
         let result = current_unit_value(unit, game_map, p2_castle, p1_castle, camp_coords);
@@ -40,7 +43,6 @@ pub fn evaluate_current_position<'a> (p2_units: &HashMap<(u32, u32), Unit<'a>>, 
         units_near_camp += result.3;
         units_able_to_attack += result.4;
     }
-    
     println!("Total value: {}\nUnits near p2 castle: {}\nUnits near p1 castle: {}\nUnits near camps: {}\nUnits able to attack: {}\n", total_value, units_defending, units_sieging, units_near_camp, units_able_to_attack);
 
     total_value
@@ -54,8 +56,8 @@ pub fn evaluate_current_position<'a> (p2_units: &HashMap<(u32, u32), Unit<'a>>, 
 // 4: able_to_attack
 // Minus "being able to attack" all other values will be calculated using heuristics (relative manhattan distance)
 // Additionally not calculating closest unit to save time since based on the distance from objectives and the ability to attack this distance should be implied
-pub fn current_unit_value<'b> (unit: &Unit<'b>, game_map: &mut GameMap, p2_castle: &(u32, u32), p1_castle: &(u32, u32), camp_coords: &Vec<(u32, u32)>) -> (u32, u32, u32, u32, u32) {    
-    let mut value: u32 = 0;
+pub fn current_unit_value<'b> (unit: &Unit<'b>, game_map: &mut GameMap, p2_castle: &(u32, u32), p1_castle: &(u32, u32), camp_coords: &Vec<(u32, u32)>) -> (f64, u32, u32, u32, u32) {    
+    let mut value: f64 = 0.0;
 
     let distance_from_own_castle = (unit.x as i32 - p2_castle.0 as i32).abs() + (unit.y as i32 - p2_castle.1 as i32).abs();
     
@@ -93,6 +95,20 @@ pub fn current_unit_value<'b> (unit: &Unit<'b>, game_map: &mut GameMap, p2_castl
                                 } else {
                                     1
                                 };
+    if defending == 0 {
+        value += distance_from_own_castle as f64 * DEFENDING_WEIGHT;
+    } 
+    if sieging == 0 {
+        value += distance_from_enemy_castle as f64 * SIEGING_WEIGHT;
+    }
+    if near_camp == 0 {
+        value += distance_from_nearest_camp as f64 * CAMP_WEIGHT;
+    }
+    if able_to_attack == 1 {
+        value + ATTACK_VALUE;
+    }
+
+    println!("Unit at {}, {}\nValue: {}, D(own_castle): {}, D(enemy_castle): {}, D(camp): {}, can_attack: {}\n", unit.x, unit.y, value, distance_from_own_castle, distance_from_enemy_castle, distance_from_nearest_camp, able_to_attack);
 
     (value, defending, sieging, near_camp, able_to_attack)
 }
