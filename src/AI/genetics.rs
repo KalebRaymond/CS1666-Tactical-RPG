4009ffd65e619cbf3ab638279764ba31a9334de2
KalebRaymond <k.raymond.form@gmail.com>
2021-11-10 15:34:03 -0500

    Converted most of the genetic algorithm pseudocode into rust

--- src/AI/genetics.rs
+++ src/AI/genetics.rs
@@ -1,69 +1,102 @@
-/*
-Will likely need a struct to keep track of individuals in a population (all units current position and the value of that state)
-Will also need to determine the best way to keep track of each units possible moves as this will be important for mutations
- 
-generate_initial_population (){
-	population = []
-	for i in 0..POP_NUM {
-		state = []
-		for each unit in units {
-			state.push(random selection of one of the units possible moves)
+use std::collections::HashMap;
+use std::convert::TryInto;
+
+use crate::AI::genetic_params::GeneticParams;
+use crate::AI::population_state::PopulationState;
+use crate::unit::Unit;
+
+//Will likely need a struct to keep track of individuals in a population (all units current position and the value of that state)
+//Will also need to determine the best way to keep track of each units possible moves as this will be important for mutations
+
+pub fn generate_initial_population(params: &GeneticParams, units: &HashMap<(u32, u32), Unit>) -> Vec<PopulationState> {
+    let mut population: Vec<PopulationState> = Vec::new();
+    
+    for i in 0..params.pop_num {
+        let mut state = PopulationState::new();
+
+		for unit in units.values() {
+			//state.push(random selection of one of the units possible moves)
 		}
-		population.push(state)
-	}
+
+		population.push(state);
+    }
+
+    population
 }
- 
-mutate (state){
-	for unit in random_sample of size MUT_NUM of state {
+
+pub fn mutate(state: &mut PopulationState) {
+	//This function depends on how we implement PopulationState
+    /*
+    for unit in random_sample of size MUT_NUM of state {
 		while new_position = current_position {
-			new_position = random selection of one of the units possible moves
+			new_position = random selection of one of the unit's possible moves
 		}
 		state.update(unit, new_position)
 	}
+    */
 }
- 
-crossover (state1, state2) {
-	randomly select bounds for state1
+
+pub fn crossover(state_1: PopulationState, state_2: PopulationState) -> (PopulationState, PopulationState) {
+    let new_state_1 = PopulationState::new();
+    let new_state_2 = PopulationState::new();
+
+    /*
+    randomly select bounds for state1
 	new_state.push(the units within these bounds from state1)
 	new_state.push(the remaining units from state2)
 	new_state2.push(the units outside the bounds of state1)
 	new_state2.push(the remaining units from state 2)
-	return new_state, new_state2
+    */
+
+    (new_state_1, new_state_2)
 }
- 
-elite_selection (current_population) {
-	num_to_keep = E_PERC * size(current_population)
-	assuming current_population is in descending order 
-	return current_population[0..num_to_keep]
+
+pub fn elite_selection(params: &GeneticParams, current_population: &mut Vec<PopulationState>) -> Vec<PopulationState> {
+	let num_to_keep: usize = ((params.e_perc * (current_population.len() as f32)).round() as i32).try_into().unwrap();
+	
+    //Assuming current_population is in descending order 
+	return current_population[0..num_to_keep].to_vec();
 }
- 
-culling (current_population) {
-	num_to_drop = C_PERC * size(current_population)
-	assuming current_population is in descending order 
-	return current_population[0..size(current_population)-num_to_drop]
+
+pub fn culling(params: &GeneticParams, current_population: &mut Vec<PopulationState>) -> Vec<PopulationState> {
+	let num_to_drop: usize = ((params.c_perc * (current_population.len() as f32)).round() as i32).try_into().unwrap();
+	
+    //Assuming current_population is in descending order 
+	return current_population[0..(current_population.len() - num_to_drop)].to_vec();
 }
- 
-genetic algorithm() {
-	init_population = generate_initial_population()
-	for i in 0..GEN_NUM {
-		new_generation.append(elite_selection(current_population))
-		remaining_population = culling(init_population)
-		generate probabilities of each individual in the remaining population to be selected (will want to weight this based on score - favor better scored states) - Boltzman distribution is commonly used, but someone more familiar with statistics feel free to suggest a different distribution
-		while size of new_generation < POP_NUM {
-			s_1 = randomly sample this distribution to get first individual
-			s_2 = randomly sample again to get second individual (ensure individuals are not the same)
-			new_individuals = crossover(s_1, s_2)
-			if size of new_generation + size of new_individuals > POP_NUM {
+
+pub fn genetic_algorithm(params: &GeneticParams, units: &HashMap<(u32, u32), Unit>, current_population: &mut Vec<PopulationState>) -> Vec<PopulationState>{
+    let mut initial_population = generate_initial_population(params, units);
+    let mut new_generation: Vec<PopulationState> = Vec::new();
+    let mut remaining_population: Vec<PopulationState> = Vec::new();
+
+    for i in 0..params.gen_num {
+        new_generation.append(&mut elite_selection(params, current_population));
+        remaining_population = culling(params, &mut initial_population);
+
+        //generate probabilities of each individual in the remaining population to be selected (will want to weight this based on score - favor better scored states) - Boltzman distribution is commonly used, but someone more familiar with statistics feel free to suggest a different distribution
+        while new_generation.len() < params.pop_num.try_into().unwrap() {
+            /*
+            state_1 = randomly sample this distribution to get first individual
+			state_2 = randomly sample again to get second individual (ensure individuals are not the same)
+            */
+            let state_1 = PopulationState::new();
+            let state_2 = PopulationState::new();
+
+            let new_individuals = crossover(state_1, state_2);
+
+            /*
+            if size of new_generation + size of new_individuals > POP_NUM {
 				add only one of the new_individuals to new_generation
 			} else {
 				add both of the new_individuals to new_generation
 			}
-		}
-		randomly sample according to MUT_PERC
-		selected_individual = mutate(selected_individual)
- 
-		init_population = new_generation
-	}
-	init_population now generally represents the best possible states that have been found and we can use these to form the considered moves of our minimax and we can repeat this for the enemy to get their "best" move and make the decision from there
+            */
+        }
+
+        initial_population = new_generation.clone();
+    }
+
+    //init_population now generally represents the best possible states that have been found and we can use these to form the considered moves of our minimax and we can repeat this for the enemy to get their "best" move and make the decision from there   
+    initial_population
 }
-*/
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate sdl2;
 #[macro_use] mod sdl_macros;
 
 
+mod AI;
 mod credits;
 mod cursor;
 mod damage_indicator;
