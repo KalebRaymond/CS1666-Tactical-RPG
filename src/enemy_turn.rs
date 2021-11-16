581198386b62f6effa6c0374d3c0b36562f8996b
AlexKwi <ajk175@pitt.edu>
2021-11-16 18:31:03 -0500

    Fixing banner issue - simple rename

--- src/enemy_turn.rs
+++ src/enemy_turn.rs
@@ -5,7 +5,7 @@ use crate::AI::*;
 use crate::game_map::GameMap;
 use crate::pixel_coordinates::PixelCoordinates;
 use crate::unit::{Team, Unit};
-use crate::turn_banner::TurnBanner;
+use crate::banner::Banner;
 use crate::SDLCore;
 
 //Utility Function Constants
@@ -17,7 +17,7 @@ const ATTACK_VALUE: f64 = 10.0;
 const MIN_DEFENSE: u32 = 5; //Since one of our AI goals says that some units should stay behind and defend, we need metrics to enforce this
 const DEFENSE_PENALTY: f64 = -500.0;
 
-pub fn handle_enemy_turn<'a, 'b>(core: &SDLCore<'b>, p2_units: &mut HashMap<(u32, u32), Unit<'a>>, p1_units: &mut HashMap<(u32, u32), Unit<'a>>, barbarian_units: &mut HashMap<(u32, u32), Unit<'a>>, game_map: &mut GameMap<'b>, turn_banner: &mut TurnBanner, current_player: &mut Team, p2_castle: &(u32, u32), p1_castle: &(u32, u32), camp_coords: &Vec<(u32, u32)>) -> Result<(), String> {
+pub fn handle_enemy_turn<'a, 'b>(core: &SDLCore<'b>, p2_units: &mut HashMap<(u32, u32), Unit<'a>>, p1_units: &mut HashMap<(u32, u32), Unit<'a>>, barbarian_units: &mut HashMap<(u32, u32), Unit<'a>>, game_map: &mut GameMap<'b>, turn_banner: &mut Banner, current_player: &mut Team, p2_castle: &(u32, u32), p1_castle: &(u32, u32), camp_coords: &Vec<(u32, u32)>) -> Result<(), String> {
     if !turn_banner.banner_visible {
         let best_moves = genetics::genetic_algorithm(p2_units, game_map, p2_castle, p1_castle, camp_coords);
         
